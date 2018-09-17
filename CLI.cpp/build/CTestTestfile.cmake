528c352b6c2b0b3487d16e95bc110108f6daf083
Jason Miesionczek <atmospherian@gmail.com>
2018-09-17 15:42:02 -0400

    remove old cpp stuff

--- CLI.cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jason/Projects/Nebula/CLI.cpp
-# Build directory: /home/jason/Projects/Nebula/CLI.cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
