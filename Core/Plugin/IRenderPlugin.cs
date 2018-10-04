d0fb30487e4e33500849da6a40cba95c63d53894
Jason Miesionczek <jmiesionczek@datto.com>
2018-10-04 16:26:33 -0400

    starting to integrate render plugin

--- Core/Plugin/IRenderPlugin.cs
+++ Core/Plugin/IRenderPlugin.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
+using Nebula.Compiler.Abstracts;
+using Nebula.Parser;
 
 namespace Core.Plugin
 {
     public interface IRenderPlugin
     {
         List<string> RenderClientImports();
+
+        List<string> RenderAbstractFunction(string url, string prefix, string returnType, string httpMethod, List<string> args);
     }
 }
